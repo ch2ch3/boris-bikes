664a3632f05584cd33a0baa9b28205bb7d0b755e
ETHEL NG <ethel.ng@gmail.com>
2014-08-12 16:40:03 +0100

    Moved docking station tests to be under bike container

---
+++ spec/bike_container_spec.rb
@@ -0,0 +1,49 @@
+require './lib/bike_container'
+
+class ContainerHolder; include BikeContainer; end
+
+describe BikeContainer do
+
+	let(:bike) { Bike.new }
+	let(:holder) { ContainerHolder.new }
+
+	it 'should know if a bike is docked to it' do
+		expect(holder.bikes).to eq []
+	end
+
+	it "should accept a bike" do
+		expect(holder.bike_count).to eq 0
+		holder.dock(bike)
+		expect(holder.bike_count).to eq 1
+	end
+
+	it "should be able to release a bike" do
+		holder.dock(bike)
+		holder.release!
+		expect(holder.bikes).to eq []
+	end
+
+	def fill_container(container)
+		10.times { container.dock(Bike.new) }
+	end
+
+	it "should know if it's full" do
+		expect(holder.full?).to be false
+		fill_container(holder)
+		expect(holder.full?).to be true
+	end
+
+	it "should not accept a bike if it's full" do
+		fill_container(holder)
+		expect(lambda { holder.dock(bike) }).to raise_error(RuntimeError)
+	end
+
+	it "should provide the list of available bikes" do
+		working_bike, broken_bike = Bike.new, Bike.new
+		broken_bike.break!
+		holder.dock(working_bike)
+		holder.dock(broken_bike)
+		expect(holder.available_bikes).to eq [working_bike]
+	end
+
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,43 +6,13 @@ describe DockingStation do
 	let(:bike) {Bike.new}
 	let(:station) {DockingStation.new(:capacity => 20)}
 
-	it 'should know if a bike is docked to it' do
-		expect(station.bikes).to eq []
-	end
-
-	it "should be able to dock a bike" do 
-		station.dock(bike)
-		expect(station.bikes).to eq [bike]
-	end
-
-	it "should be able to release a bike" do
-		station.dock(bike)
-		station.release!
-		expect(station.bikes).to eq []
-	end
-
-	def fill_station(station)
-		20.times { station.dock(Bike.new) }
-	end
-
-	it "should know if it's full" do
-		expect(station.full?).to be false
-		fill_station(station)
-		expect(station.full?).to be true
-	end
-
-	it "should not accept a bike if it's full" do
-		fill_station(station)
-		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
-	end
-
-	it "should provide the list of available bikes" do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break!
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq [working_bike]
-	end
+	# it "should provide the list of available bikes" do
+	# 	working_bike, broken_bike = Bike.new, Bike.new
+	# 	broken_bike.break!
+	# 	station.dock(working_bike)
+	# 	station.dock(broken_bike)
+	# 	expect(station.available_bikes).to eq [working_bike]
+	# end
 
 	it "should not release a broken bike" do
 		bike.break!
@@ -50,26 +20,5 @@ describe DockingStation do
 		station.release!
 		expect(station.bikes).to eq [bike]
 	end
-
-	# it 'should know if a bike has been released' do
-	# 	dock.remove_bike!
-	# 	expect(dock.has_bike?).to eq false
-	# end
-
-	# it 'should release a working bike to a person' do
-	# 	dock.has_bike? && bike.work?
-	# 	expect(dock.release_bike?).to be true
-	# end
-
-	# it 'should not release a broken bike to a person' do
-	# 	dock.has_bike? && bike.break!
-	# 	expect(dock.release_bike?).to be false
-	# end
-
-	# it 'should be able to receive a bike' do
-	# 	dock.remove_bike!
-	# 	dock.receive_bike!
-	# 	expect(dock.has_bike?).to eq true
-	# end
 
 end
